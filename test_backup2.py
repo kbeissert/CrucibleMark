a3eb6ffb2ebd8e17808a9ef0de112bfa6d99ced8
Kay Beißert <kay.beissert@media-garage.de>
2026-03-23 19:04:42 +0100

    Chore: Remove temporary ad-hoc scripts and log files

--- test_backup2.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-with open("Makefile", "r") as f:
-    text = f.read()
-import re
-target = re.search(r'clean-runs.*?:.*?(?=\n\w|\Z)', text, re.DOTALL)
-if target:
-    print(target.group(0))
